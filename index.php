adbea4652c41abd9d3e369577c575f21327c9fa9
Aaron Oxborrow <aaron@pastedesign.com>
2013-04-23 08:16:14 -0700

    initial commit - example site w/Paste CMS

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// setup error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+// check PHP version
+version_compare(PHP_VERSION, '5.3', '<') and exit('Paste requires PHP 5.3 or newer.');
+
+// composer autoload
+// require 'vendor/autoload.php';
+
+// need this one
+require '../Paste/src/Paste/Paste.php';
+
+// namespace shortcut
+use Paste\Paste;
+
+// user defined routing
+// 'route regex' => any valid callback
+// matched tokens from the regex will be passed as parameters
+// e.g. 'blog/post/([A-Za-z0-9]+)' => 'Class::method',
+Paste::$routes = array(
+	// example user defined blog route
+	'blog/post/([A-Za-z0-9-_]+)' => function($slug) { 
+		echo "Example callback route, slug: <b>$slug</b><br/>";
+	},
+);
+
+Paste::run();
+

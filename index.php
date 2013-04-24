d8394bb24c0c917eef68fcccd5e0b53e391ffb5f
Aaron Oxborrow <aaron@pastedesign.com>
2013-04-23 20:36:09 -0700

    cleaner routing example

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ ini_set('display_errors', TRUE);
 version_compare(PHP_VERSION, '5.3', '<') and exit('Paste requires PHP 5.3 or newer.');
 
 // composer autoload
-// require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
 // temp during development
-require '../Paste/src/Paste/Paste.php';
+// require '../Paste/src/Paste/Paste.php';
 
 // namespace shortcut
 use Paste\Paste;
@@ -26,10 +26,9 @@ use Paste\Paste;
 // $paste->run();
 
 // instantiate Paste singleton
-Paste::instance()
+Paste::instance()->route(
 	// example user defined blog route
-	->route('blog/post/([A-Za-z0-9-_]+)', function($slug) { 
+	'blog/post/([A-Za-z0-9-_]+)', function($slug) { 
 		echo "Example callback route, slug: <b>$slug</b><br/>";
-	})
-	->run();
+	})->run();
 

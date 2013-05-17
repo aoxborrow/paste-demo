f1e7402c2eaa26eef9b64adad11f168a7cfba849
Aaron Oxborrow <aaron@pastedesign.com>
2013-05-17 15:59:40 -0700

    update example to use static init

--- index.php
+++ index.php
@@ -10,25 +10,20 @@ version_compare(PHP_VERSION, '5.3', '<') and exit('Paste requires PHP 5.3 or new
 // composer autoload
 require 'vendor/autoload.php';
 
-// temp during development
-// require '../Paste/src/Paste/Paste.php';
-
 // namespace shortcut
 use Paste\Paste;
+
+// bare minimum
+// Paste\Paste::run();
 
 // user defined routing
 // 'route regex' => any valid callback
 // matched tokens from the regex will be passed as parameters
 // e.g. 'blog/post/([A-Za-z0-9]+)' => 'Class::method',
-
-// bare minimum
-// $paste = new Paste\Paste;
-// $paste->run();
-
-// instantiate Paste singleton
-Paste::instance()->route(
-	// example user defined blog route
+Paste::route(
 	'blog/post/([A-Za-z0-9-_]+)', function($slug) { 
 		echo "Example callback route, slug: <b>$slug</b><br/>";
-	})->run();
+	});
+
+Paste::run();
 

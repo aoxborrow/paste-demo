15fdc27ebc6c6aef774f37f63ef42133562c2e5e
Aaron Oxborrow <aaron@pastedesign.com>
2013-06-03 18:52:50 -0700

    finalize blog structure, styling and template updates

--- index.php
+++ index.php
@@ -14,10 +14,14 @@ use Paste\Paste;
 // (optional) user defined routing
 // 'route regex' => any valid callback
 // matched tokens from the regex will be passed as parameters
-// e.g. 'blog/post/([A-Za-z0-9]+)' => 'Class::method',
-Paste::route('blog/post/([A-Za-z0-9-_]+)', function($slug) { 
-	echo "Example callback route, slug: <b>$slug</b><br/>";
+// e.g. 'blog/([A-Za-z0-9-]+)' => 'Class::method',
+Paste::route('blog/([0-9]{4})/([0-9]{2})/([0-9]{2})/([A-Za-z0-9-]+)', function($year, $month, $day, $name) { 
+	// strip date and run normal content request
+	Paste::content_request("blog/$name");
+
 });
+
+// debugging route
 Paste::route('debug', function() { 
 	Paste::debug();
 });

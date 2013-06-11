9b51550806d10d968d4498ca5cc570a5d45135d0
Aaron Oxborrow <aaron@pastedesign.com>
2013-06-10 17:57:26 -0700

    use new Content class for blog demo

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+// shortcut
+namespace Paste;
+
 // setup error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
@@ -9,7 +12,6 @@ version_compare(PHP_VERSION, '5.3', '<') and exit('Paste requires PHP 5.3 or new
 
 // composer autoload
 require 'vendor/autoload.php';
-use Paste\Paste;
 
 // (optional) user defined routing
 // 'route regex' => any valid callback
@@ -17,7 +19,7 @@ use Paste\Paste;
 // e.g. 'blog/([A-Za-z0-9-]+)' => 'Class::method',
 Paste::route('blog/([0-9]{4})/([0-9]{2})/([0-9]{2})/([A-Za-z0-9-]+)', function($year, $month, $day, $name) { 
 	// strip date and run normal content request
-	Paste::content_request("blog/$name");
+	Content::request("blog/$name");
 });
 
 // init routing and run

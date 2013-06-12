76bd792cb1dccb106f70d49016e991bd9f379d26
Aaron Oxborrow <aaron@pastedesign.com>
2013-06-11 19:48:38 -0700

    allow underscore in blog slugs

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ require 'vendor/autoload.php';
 // (optional) user defined routing
 // 'route regex' => any valid callback
 // matched tokens from the regex will be passed as parameters
-// e.g. 'blog/([A-Za-z0-9-]+)' => 'Class::method',
-Paste::route('blog/([0-9]{4})/([0-9]{2})/([0-9]{2})/([A-Za-z0-9-]+)', function($year, $month, $day, $name) { 
+// e.g. 'blog/([A-Za-z0-9-_]+)' => 'Class::method',
+Paste::route('blog/([0-9]{4})/([0-9]{2})/([0-9]{2})/([A-Za-z0-9-_]+)', function($year, $month, $day, $name) { 
 	// strip date and run normal content request
 	Content::request("blog/$name");
 });

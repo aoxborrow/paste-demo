9eaeb6b4c9905d55d71d53e7172bb4deecaeee36
Aaron Oxborrow <aoxborrow@lynchmarks.com>
2016-07-29 12:17:08 -0700

    changes to demo site based on Paste refactor for base_url

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-![Screenshot](http://paste-demo.pastelabs.com/assets/images/demo-site.png)  
+![Screenshot](https://github.com/paste/paste-demo/raw/master/assets/images/demo-site.png)  
 This is a demo site for [Paste](https://github.com/paste/Paste) -- a lightweight "CMS" built around static files and folders instead of a database.
-#### http://paste-demo.pastelabs.com
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-// shortcut
-namespace Paste;
-
 // setup error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
@@ -12,16 +9,31 @@ version_compare(PHP_VERSION, '5.3', '<') and exit('Paste requires PHP 5.3 or new
 
 // composer autoload
 require 'vendor/autoload.php';
+use Paste\Paste;
+
+// configuration
+$config = array(
+	// optionally specify a 'base_url' if serving Paste from a subdirectory
+	// 'base_url' => 'paste-demo',
+	// relative path to content directory
+	// 'content_dir' => 'content',
+	// relative path to template directory
+	// 'template_dir' => 'templates',
+	// relative path to cache directory
+	// 'cache_dir' => 'cache',
+);
+
+// load config and parse content directory
+$paste = new Paste($config);
 
 // (optional) user defined routing
 // 'route regex' => any valid callback
-// matched tokens from the regex will be passed as parameters
-// e.g. 'blog/([A-Za-z0-9-_]+)' => 'Class::method',
-Paste::route('blog/([0-9]{4})/([0-9]{2})/([0-9]{2})/([A-Za-z0-9-_]+)', function($year, $month, $day, $name) { 
+// matched tokens from the regex will be passed as parameters, with $paste instance first
+// e.g. 'blog/([A-Za-z0-9-_]+)' => 'function($paste, $slug) { ... }',
+$paste->add_route('blog/([0-9]{4})/([0-9]{2})/([0-9]{2})/([A-Za-z0-9-_]+)', function($paste, $year, $month, $day, $name) {
 	// strip date and run normal content request
-	Content::request("blog/$name");
+	return $paste->render_url("blog/$name");
 });
 
 // init routing and run
-Paste::run();
-
+$paste->run();
